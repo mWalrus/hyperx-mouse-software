ceeb015a27b4de238057f4845a16acc16a064d82
mWalrus <hellkvistoskar@protonmail.com>
2023-02-27 22:48:10 +0100

    chore: clean up

--- src/command.rs
+++ src/command.rs
@@ -3,43 +3,34 @@ pub struct Command {
 }
 
 impl Command {
+    fn construct(data: Vec<u8>) -> Self {
+        Self { data }.pad()
+    }
+
     pub fn set_color(color: &[u8; 3]) -> Self {
-        let data: Vec<u8> = vec![
+        Self::construct(vec![
             0xd2, 0x00, 0x00, 0x08, color[0], color[1], color[2], color[0], color[1], color[2],
             0x64,
-        ];
-        Self { data }.pad()
+        ])
     }
 
     // informs device to persist all changes
     pub fn persist() -> Self {
-        Self {
-            data: vec![0xde, 0xff],
-        }
-        .pad()
+        Self::construct(vec![0xde, 0xff])
     }
 
     pub fn set_dpi_profile_dpi(id: u8, dpi: u8) -> Self {
-        Self {
-            data: vec![0xd3, 0x02, id, 0x02, dpi],
-        }
-        .pad()
+        Self::construct(vec![0xd3, 0x02, id, 0x02, dpi])
     }
 
     pub fn set_dpi_profile_color(id: u8, color: [u8; 3]) -> Self {
-        Self {
-            data: vec![0xd3, 0x03, id, 0x03, color[0], color[1], color[2]],
-        }
-        .pad()
+        Self::construct(vec![0xd3, 0x03, id, 0x03, color[0], color[1], color[2]])
     }
 
     pub fn __unknown_command_1() -> Self {
-        Self {
-            data: vec![
-                0xd2, 0x00, 0x00, 0x08, 0x9b, 0x00, 0xdc, 0x9b, 0x00, 0xdc, 0x64,
-            ],
-        }
-        .pad()
+        Self::construct(vec![
+            0xd2, 0x00, 0x00, 0x08, 0x9b, 0x00, 0xdc, 0x9b, 0x00, 0xdc, 0x64,
+        ])
     }
 
     fn pad(mut self) -> Self {
