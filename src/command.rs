ccecb77da6b5f2790af198824c340fa84c8b9e6a
mWalrus <oskar.hellkvist@rapidimages.se>
2023-02-28 09:11:41 +0100

    fix: clean up command construction

--- src/command.rs
+++ src/command.rs
@@ -3,8 +3,14 @@ pub struct Command {
 }
 
 impl Command {
-    fn construct(data: Vec<u8>) -> Self {
-        Self { data }.pad()
+    fn construct(mut data: Vec<u8>) -> Self {
+        let len = data.len();
+        let required_padding = (8 - (len % 8)) % 8;
+
+        for _ in 0..required_padding {
+            data.push(0x00);
+        }
+        Self { data }
     }
 
     pub fn set_color(color: &[u8; 3]) -> Self {
@@ -25,21 +31,5 @@ impl Command {
 
     pub fn set_dpi_profile_color(id: u8, color: [u8; 3]) -> Self {
         Self::construct(vec![0xd3, 0x03, id, 0x03, color[0], color[1], color[2]])
-    }
-
-    pub fn __unknown_command_1() -> Self {
-        Self::construct(vec![
-            0xd2, 0x00, 0x00, 0x08, 0x9b, 0x00, 0xdc, 0x9b, 0x00, 0xdc, 0x64,
-        ])
-    }
-
-    fn pad(mut self) -> Self {
-        let len = self.data.len();
-        let required_padding = (8 - (len % 8)) % 8;
-
-        for _ in 0..required_padding {
-            self.data.push(0x00);
-        }
-        self
     }
 }
