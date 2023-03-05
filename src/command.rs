33a44c43ac69ea587b88db74572e0c1667ad97d4
mWalrus <hellkvistoskar@protonmail.com>
2023-03-05 14:23:08 +0100

    feat: add command macro

--- src/command.rs
+++ src/command.rs
@@ -1,41 +1,32 @@
+use crate::command;
+
 #[derive(Debug)]
 pub struct Command {
     pub data: Vec<u8>,
 }
 
 impl Command {
-    fn construct(mut data: Vec<u8>) -> Self {
-        let len = data.len();
-        let required_padding = (8 - (len % 8)) % 8;
-
-        for _ in 0..required_padding {
-            data.push(0x00);
-        }
-        Self { data }
-    }
-
     pub fn set_color(color: [u8; 3], opacity: u8) -> Self {
-        Self::construct(vec![
+        command!(
             0xd2, 0x00, 0x00, 0x08, color[0], color[1], color[2], color[0], color[1], color[2],
-            opacity,
-        ])
+            opacity
+        )
     }
 
-    // informs device to persist all changes
     pub fn persist() -> Self {
-        Self::construct(vec![0xde, 0xff])
+        command!(0xde, 0xff)
     }
 
     pub fn set_dpi_profile_dpi(id: u8, dpi: u8) -> Self {
-        Self::construct(vec![0xd3, 0x02, id, 0x02, dpi])
+        command!(0xd3, 0x02, id, 0x02, dpi)
     }
 
     pub fn set_dpi_profile_color(id: u8, color: [u8; 3]) -> Self {
-        Self::construct(vec![0xd3, 0x03, id, 0x03, color[0], color[1], color[2]])
+        command!(0xd3, 0x03, id, 0x03, color[0], color[1], color[2])
     }
 
     pub fn set_polling_rate(rate: u8) -> Self {
-        Self::construct(vec![0xd0, 0x00, 0x00, 0x01, rate])
+        command!(0xd0, 0x00, 0x00, 0x01, rate)
     }
 
     pub fn set_gradient_part(part: u8, colors: [[u8; 3]; 20]) -> Self {
@@ -45,10 +36,51 @@ impl Command {
                 data.push(channel);
             }
         }
-        Self::construct(data)
+        Command {
+            data: crate::pad!(data),
+        }
     }
 
     pub fn set_low_power_warn(percentage: u8) -> Self {
-        Self::construct(vec![0xd1, 0x00, 0x00, 0x01, percentage])
+        command!(0xd1, 0x00, 0x00, 0x01, percentage)
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{command, Command};
+
+    #[test]
+    fn should_pad_to_8_bytes() {
+        let cmd = command!(0x00);
+        assert_eq!(8, cmd.data.len());
+    }
+
+    #[test]
+    fn should_not_pad() {
+        let cmd = command!(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
+        assert_eq!(8, cmd.data.len());
+    }
+}
+
+#[macro_export]
+macro_rules! command {
+    ($($byte:expr),*) => {
+        {
+            Command { data: crate::pad!(vec![$($byte),*]) }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! pad {
+    ($v:expr) => {{
+        let len = $v.len();
+        let required_padding = (8 - (len % 8)) % 8;
+
+        for _ in 0..required_padding {
+            $v.push(0x00);
+        }
+        $v
+    }};
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[macro_use]
 mod command;
 mod mouse;
 mod polling_rate;
